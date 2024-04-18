5e1125a1620d703fda5017d05688e96fdeb23f1b
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-18 11:51:27 +0700

    Update 2_Team_Detailed.py

--- pages/2_Team_Detailed.py
+++ pages/2_Team_Detailed.py
@@ -15,3 +15,70 @@ from matplotlib.patches import FancyBboxPatch
 
 from fungsiplot import plot_skuad
 from fungsiplot import plot_skuadbar
+
+
+
+st.set_page_config(page_title='Team Detailed', layout='wide')
+st.markdown('# Team Detailed')
+
+col1, col2, col3 = st.columns(3)
+with col1:
+  komp = st.selectbox('Select Competition', ['Liga 1', 'Liga 2'], key='50')
+with col2:
+  smt = fulldata[fulldata['Kompetisi']==komp]
+  team = st.selectbox('Select Team', pd.unique(smt['Team']), key='51')
+with col3:
+  smt = smt[smt['Team']==team]
+  gw = st.multiselect('Select GWs', pd.unique(smt['Gameweek']), key='52')
+  all_gws = st.checkbox('Select All GWs', key='307')
+  if all_gws:
+    gw = pd.unique(smt['Gameweek'])
+ds = get_pssw(fulldata, th, team, gw)
+ds = ds.replace('', pd.NA)
+ps = ['PS1','PS2','PS3','PS4','PS5']
+s = ['S1','S2','S3','S4','S5','S6','S7']
+w = ['W1','W2','W3','W4','W5','W6','W7']
+
+col1, col2 = st.columns(2)
+with col1:
+  st.subheader(team+'\'s Results')
+  rslt = get_wdl(fulldata, team, gw)
+  st.dataframe(rslt, hide_index=True)
+with col2:
+  st.subheader(team+'\'s Squad List')
+  skd = get_skuad(df1, df2, team, gw)
+  st.dataframe(skd)
+
+st.subheader(team+'\'s Squad - % of Minutes Played')
+col1, col2 = st.columns(2)
+with col1:
+  skbr = plot_skuadbar(df1, df2, team, gw)
+  st.pyplot(skbr)
+with col2:
+  sksc = plot_skuad(df1, df2, team, gw)
+  st.pyplot(sksc)
+
+st.subheader(team+'\'s Characteristics')
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.markdown('**Play Style**')
+  for col in ds[ps]:
+    if (ds[col].isnull().values.any() == False):
+      st.markdown(':large_yellow_square:'+' '+list(ds[col])[0])
+with col2:
+  st.markdown('**Strengths**')
+  for col in ds[s]:
+    if (ds[col].isnull().values.any() == False):
+      st.markdown(':large_green_square:'+' '+list(ds[col])[0])
+with col3:
+  st.markdown('**Weaknesses**')
+  for col in ds[w]:
+    if (ds[col].isnull().values.any() == False):
+      st.markdown(':large_red_square:'+' '+list(ds[col])[0])
+
+st.subheader(team+'\'s Starting Formation')
+gw2 = st.selectbox('Select GW', pd.unique(smt['Gameweek']), key='53')
+full_form = get_formasi(df1, cd)
+sf = plot_form(full_form, cf, team, gw2)
+
+st.pyplot(sf)
