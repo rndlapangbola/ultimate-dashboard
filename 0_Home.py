0fd660cf9963d2bd28a22258ecf211f81acad493
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-18 15:08:26 +0700

    Update 0_Home.py

--- 0_Home.py
+++ 0_Home.py
@@ -9,19 +9,16 @@ def run():
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Dashboard ini dibuat oleh **Prana dari R&D Lapangbola** untuk mempermudah akses 
+        data-data yang telah diolah baik untuk konten maupun laporan ke LIB serta 
+        dapat dipergunakan untuk plotting (xG map, passing network, dll).
+        **👈 Pilih fitur pada sidebar** untuk melihat dan 
+        menggunakan fitur-fitur dashboard ini.
+        ### Terima kasih kepada:
+        - Kang Dani dan Dzikry,
+        - Tim Operasional,
+        - Tim Konten,
+        - Serta seluruh elemen Lapangbola.
     """
     )
 
