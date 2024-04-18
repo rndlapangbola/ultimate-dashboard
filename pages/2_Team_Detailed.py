0c4e145df7a1ccd32ceba56f5ea5f1a8b2c67eaf
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-18 11:52:59 +0700

    Update 2_Team_Detailed.py

--- pages/2_Team_Detailed.py
+++ pages/2_Team_Detailed.py
@@ -24,7 +24,21 @@ from listfungsi import get_detail
 st.set_page_config(page_title='Team Detailed', layout='wide')
 st.markdown('# Team Detailed')
 
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    xlsx_url = sheets_url.replace("/edit#gid=", "/export?format=xlsx&gid=")
+    return pd.read_excel(xlsx_url)
 
+df1 = load_data(st.secrets["datateam"])
+df2 = load_data(st.secrets["datapemain"])
+
+from datetime import date
+df1['Date'] = pd.to_datetime(df1.Date)
+df1['Month'] = df1['Date'].dt.strftime('%B')
+df22 = get_detail(df2)
+df = pd.merge(df1, df2.drop(['Name'], axis=1), on='Player ID', how='left')
+fulldata = get_detail(df)
+mlist = get_list(fulldata)
 
 col1, col2, col3 = st.columns(3)
 with col1:
