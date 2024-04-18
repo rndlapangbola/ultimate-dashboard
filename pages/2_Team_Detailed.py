2e71dcd35ba089652632a0f7f344cdbc8597a38b
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-18 11:42:42 +0700

    Update 2_Team_Detailed.py

--- pages/2_Team_Detailed.py
+++ pages/2_Team_Detailed.py
@@ -1,1 +1,19 @@
+import sys
+import io
+import streamlit as st
+import pandas as pd
+import numpy as np
+from tempfile import NamedTemporaryFile
+import urllib
 
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
+import matplotlib.patches as patches
+from matplotlib.offsetbox import (OffsetImage, AnnotationBbox)
+import matplotlib.font_manager as fm
+from matplotlib.patches import FancyBboxPatch
+
+sys.path.append("fungsiplot.py")
+import fungsiplot
+from fungsiplot import plot_skuad
+from fungsiplot import plot_skuadbar
