17a0d9c46332819d6ab07896cccb63825e80a5b3
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-18 16:15:29 +0700

    Update 0_Home.py

--- 0_Home.py
+++ 0_Home.py
@@ -14,6 +14,16 @@ def run():
         dapat dipergunakan untuk plotting (xG map, passing network, dll).
         **👈 Pilih fitur pada sidebar** untuk melihat dan 
         menggunakan fitur-fitur dashboard ini.
+
+        ### Fitur:
+        - **Season Statistics**: Berisi milestones tim di Liga 1 serta statistik tim dan pemain di musim ini.
+        - **Team Detailed**: Berisi statistik dan visualisasi data tim secara lebih detail.
+        - **Player Detailed**: Berisi statistik dan visualisasi data pemain secara lebih detail.
+        - **Player Search**: Untuk membuat list pemain terbaik sesuai kriteria yang dibutuhkan.
+        - **Plotting xG**: Untuk plotting xG map dan assist/key pass map.
+        - **Plotting Passing Network**: Untuk plotting passing network.
+        - **Excel-to-XML Converter*: Untuk mengkonversi file timeline (.xlsx) ke format XML untuk video tagging.
+        
         ### Terima kasih kepada:
         - Kang Dani dan Dzikry,
         - Tim Operasional,
