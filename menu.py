2057d79555e546392c750b356b594b8d6f7c5b71
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-24 14:45:25 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,17 +2,23 @@ import streamlit as st
 
 def authenticated_menu():
     # Show a navigation menu for authenticated users
-    st.sidebar.page_link("pages/1_Home.py", label="Home")
-    st.sidebar.page_link("pages/2_Season_Statistics.py", label="Statistics")
-    st.sidebar.page_link("pages/3_Log_Out.py", label="Log Out")
+    st.sidebar.page_link("pages/1_Home_Page.py", label="Home")
+    st.sidebar.page_link("pages/2_Match_Center.py", label="Match Center")
+    st.sidebar.page_link("pages/3_Season_Statistics.py", label="Season Statistics")
+    st.sidebar.page_link("pages/4_Team_Detailed.py", label="Team Detailed")
+    st.sidebar.page_link("pages/5_Player_Detailed.py", label="Player Detailed")
+    st.sidebar.page_link("pages/6_Player_Search.py", label="Player Search")
+    st.sidebar.page_link("pages/7_Plotting_xG.py", label="Plotting xG")
+    st.sidebar.page_link("pages/8_Plotting_Passing_Network.py", label="Plotting Passing Network")
+    st.sidebar.page_link("pages/9_Excel-to-XML_Converter.py", label="Excel-to-XML Converter")
 
 def unauthenticated_menu():
     # Show a navigation menu for unauthenticated users
-    st.switch_page("app.py")
+    st.switch_page("0_Home.py")
 
 def home_menu():
     # Show a navigation menu for unauthenticated users
-    st.switch_page("pages/1_Home.py")
+    st.switch_page("pages/1_Home_Page.py")
 
 def menu():
     return authenticated_menu()
