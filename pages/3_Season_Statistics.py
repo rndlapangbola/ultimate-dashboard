136eec63f45267c64251b2473435f9036432422b
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-24 15:00:46 +0700

    Update 3_Season_Statistics.py

--- pages/3_Season_Statistics.py
+++ pages/3_Season_Statistics.py
@@ -12,11 +12,11 @@ from listfungsi import get_list
 from listfungsi import get_detail
 from listfungsi import get_cs
 from listfungsi import milestone
-from menu import menu
 
 st.set_page_config(page_title='Full Season Statistics', layout='wide')
 st.markdown('# Statistics')
 
+from menu import menu
 menu()
 
 @st.cache_data(ttl=600)
