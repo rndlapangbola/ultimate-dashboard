f5f53f4dcac24b9bb6e0ad6328e8de8a52555de6
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-05-16 13:13:11 +0700

    Update 11_Plotting_Tools.py

--- pages/11_Plotting_Tools.py
+++ pages/11_Plotting_Tools.py
@@ -1,1 +1,11 @@
-cc
+import sys
+import pandas as pd
+import numpy as np
+import streamlit as st
+import io
+
+st.set_page_config(page_title='Plotting Tools', layout='wide')
+st.markdown('# Plotting Tools')
+
+from menu import menu
+menu()
