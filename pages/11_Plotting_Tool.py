590a554b448592c64c665337a0a9e98401d3d881
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-05-16 13:16:09 +0700

    Update and rename 11_Plotting_Tools.py to 11_Plotting_Tool.py

--- pages/11_Plotting_Tool.py
+++ pages/11_Plotting_Tool.py
@@ -4,8 +4,8 @@ import numpy as np
 import streamlit as st
 import io
 
-st.set_page_config(page_title='Plotting Tools', layout='wide')
-st.markdown('# Plotting Tools')
+st.set_page_config(page_title='Plotting Tool', layout='wide')
+st.markdown('# Plotting Tool')
 
 from menu import menu
 menu()
