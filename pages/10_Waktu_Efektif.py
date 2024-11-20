322f90eb5a7c07ebb9e51f342242f37a1a09a427
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-11-20 13:07:56 +0700

    Update 10_Waktu_Efektif.py

--- pages/10_Waktu_Efektif.py
+++ pages/10_Waktu_Efektif.py
@@ -65,23 +65,21 @@ with col2:
         mime='application/vnd.ms-excel',
         key = 1)
 
-'''
-if 'coor' not in st.session_state:
-    st.session_state['coor'] = []
+#if 'coor' not in st.session_state:
+#    st.session_state['coor'] = []
 
-xval = 617.6470588235249
-yval = 400
+#xval = 617.6470588235249
+#yval = 400
 
-value = streamlit_image_coordinates('./data/Lapangkosong.jpg', width=xval, height=yval, key="local",)
+#value = streamlit_image_coordinates('./data/Lapangkosong.jpg', width=xval, height=yval, key="local",)
 
-if value is not None:
-  coor = value['x'], value['y']
-  if coor not in st.session_state['coor']:
-    st.session_state['coor'].append(coor)
-    st.experimental_rerun()
-df = pd.DataFrame(st.session_state['coor'])
-df = df.rename(columns={df.columns[0]:'X',df.columns[1]:'Y'})
-df['X'] = (df['X']*100)/xval
-df['Y'] = df['Y']/4
-edited_df = st.data_editor(df, num_rows="dynamic")
-'''
+#if value is not None:
+#  coor = value['x'], value['y']
+#  if coor not in st.session_state['coor']:
+#    st.session_state['coor'].append(coor)
+#    st.experimental_rerun()
+#df = pd.DataFrame(st.session_state['coor'])
+#df = df.rename(columns={df.columns[0]:'X',df.columns[1]:'Y'})
+#df['X'] = (df['X']*100)/xval
+#df['Y'] = df['Y']/4
+#edited_df = st.data_editor(df, num_rows="dynamic")
